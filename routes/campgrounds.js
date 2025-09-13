644dca3e07b06d4df8bde90b432063818063b7d8
Denwa-N <h.nomoto.14@gmail.com>
2025-09-13 12:36:24 +0900

    feat(auth): add login status check

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schemas');
-
+const { isLoggedIn } = require('../middleware');
 
 // スキーマバリデーションミドルウェア
 const validateCampground = (req, res, next) => {
@@ -25,12 +25,12 @@ router.get('/', catchAsync(async (req, res) => {
 }));
 
 // キャンプ場の新規登録画面へのルートの定義
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 });
 
 // キャンプ場の新規登録処理のルートの定義
-router.post('/', validateCampground, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     // 新しいキャンプ場を作成したときフラッシュを設定する
@@ -50,7 +50,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 }));
 
 // キャンプ場の編集画面へのルートの定義
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     // 存在しない or 削除済みのキャンプ場へアクセスしようとした際のエラー処理
     if (!campground) {
@@ -61,7 +61,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 }));
 
 // キャンプ場の編集処理のルートの定義
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     // キャンプ場を更新したときのフラッシュを設定する
@@ -70,7 +70,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 }));
 
 // キャンプ場の削除処理のルートの定義
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     // キャンプ場を削除したときフのラッシュを設定する
